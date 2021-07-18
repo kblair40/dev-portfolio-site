bd5704389b543162f681b98515572c22465e062a
Kevin Blair <kblair40@gmail.com>
2021-07-18 08:38:32 -0500

    Fix scroll container area sizing

--- src/components/UI/ProjectCards/ProjectCards.js
+++ src/components/UI/ProjectCards/ProjectCards.js
@@ -1,31 +1,50 @@
 import React from "react";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
+import "react-perfect-scrollbar/dist/css/styles.css";
+import PerfectScrollbar from "react-perfect-scrollbar";
 
 import ProjectCard from "./ProjectCard";
 import classes from "./ProjectCards.module.css";
 
 const ProjectCards = () => {
+  const mdScreen = useMediaQuery("(max-width: 890px)");
+
+  const logEnd = () => {
+    console.log("end");
+  };
+
   return (
-    <div className={classes.container}>
-      <ProjectCard
-        name="ecommerce"
-        liveLink="https://ecommerce-kb.netlify.app/"
-        githubLink="https://github.com/kblair40/ecommerce"
-      />
-      <ProjectCard
-        name="user auth"
-        liveLink="https://signup-kb.netlify.app/signup"
-        githubLink="https://github.com/kblair40/signup"
-      />
-      <ProjectCard
-        name="memory"
-        liveLink="https://memory-6fc80b.netlify.app/"
-        githubLink="https://github.com/kblair40/memory"
-      />
-      <ProjectCard
-        name="todos"
-        liveLink="https://todos-kb.netlify.app/"
-        githubLink="https://github.com/kblair40/Todo"
-      />
+    <div>
+      <PerfectScrollbar
+        className={classes.scrollbar}
+        options={{
+          suppressScrollX: true,
+        }}
+        onYReachEnd={logEnd}
+      >
+        <div className={classes.container}>
+          <ProjectCard
+            name="ecommerce"
+            liveLink="https://ecommerce-kb.netlify.app/"
+            githubLink="https://github.com/kblair40/ecommerce"
+          />
+          <ProjectCard
+            name="user auth"
+            liveLink="https://signup-kb.netlify.app/signup"
+            githubLink="https://github.com/kblair40/signup"
+          />
+          <ProjectCard
+            name="memory"
+            liveLink="https://memory-6fc80b.netlify.app/"
+            githubLink="https://github.com/kblair40/memory"
+          />
+          <ProjectCard
+            name="todos"
+            liveLink="https://todos-kb.netlify.app/"
+            githubLink="https://github.com/kblair40/Todo"
+          />
+        </div>
+      </PerfectScrollbar>
     </div>
   );
 };
--- src/components/component-pages/WorkPage.js
+++ src/components/component-pages/WorkPage.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import Slide from "@material-ui/core/Slide";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
 import { useSelector } from "react-redux";
 
 import UpArrow from "../UI/Arrows/UpArrow";
@@ -8,7 +9,8 @@ import classes from "./WorkPage.module.css";
 
 const WorkPage = ({ disableScrolling, enableScrolling }) => {
   const workIsHidden = useSelector((state) => state.hidden.work);
-
+  const mdScreen = useMediaQuery("(max-width: 890px)");
+  console.log("MEDIUM SCREEN:", mdScreen);
   return (
     <div>
       <Slide
