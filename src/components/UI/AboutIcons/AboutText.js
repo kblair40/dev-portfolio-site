d9c639b67b4a6f4574c0a6f86d2abdfa638e3fcb
Kevin Blair <kblair40@gmail.com>
2021-07-19 15:46:32 -0500

    Finish AboutPage media queries

--- src/components/UI/AboutIcons/AboutText.js
+++ src/components/UI/AboutIcons/AboutText.js
@@ -8,14 +8,14 @@ const AboutText = () => {
       <div className={classes.textContainer}>
         <div className={classes.column}>
           <h3 className={classes.shortAbout}>
-            Hi, I'm Kevin. Nice to meet you. Please have a look around.
+            Hi, I'm Kevin. Nice to meet you. Please have a look around!
           </h3>
         </div>
         <div className={classes.spacer} />
         <div className={classes.column}>
           <p className={classes.longAbout}>
             I'm a technophile and web developer in and from Chicago specializing
-            in highly efficient React apps and CSS/HTML that work on all
+            in highly efficient React apps and CSS/HTML that works on all
             platforms and browsers. &nbsp;I generally prefer Jamstack solutions
             but will gladly dabble in NodeJS (Express) and databases such as
             MongoDB and Firebase as needed.
