01cb1d40703f733f73b4af3210c5de6dd0ad1fdd
Kevin Blair <kblair40@gmail.com>
2021-07-27 16:08:04 -0500

    Make all ResumeSection components responsive

--- src/components/Resume/ProjectLinks.js
+++ src/components/Resume/ProjectLinks.js
@@ -14,36 +14,37 @@ const ProjectLinks = ({
   const isDarkMode = useSelector((st) => st.theme.isDarkMode);
   return (
     <div className={classes.subheader}>
+      <h4>{subheader}</h4>
       <div className={classes.subheaderContent}>
-        <h4>{subheader}</h4>
         <p>
           <i className={classes.detail}>{detail}</i>
         </p>
+        {/* </div> */}
+        {liveLink && githubLink && (
+          <div className={classes.links}>
+            <a
+              className={classNames(
+                classes.liveLink,
+                isDarkMode ? classes.linkDark : classes.linkLight
+              )}
+              href={liveLink}
+              target="blank"
+            >
+              Live
+            </a>
+            <a
+              className={classNames(
+                classes.githubLink,
+                isDarkMode ? classes.linkDark : classes.linkLight
+              )}
+              href={githubLink}
+              target="blank"
+            >
+              Github
+            </a>
+          </div>
+        )}
       </div>
-      {liveLink && githubLink && (
-        <div className={classes.links}>
-          <a
-            className={classNames(
-              classes.liveLink,
-              isDarkMode ? classes.linkDark : classes.linkLight
-            )}
-            href={liveLink}
-            target="blank"
-          >
-            Live
-          </a>
-          <a
-            className={classNames(
-              classes.githubLink,
-              isDarkMode ? classes.linkDark : classes.linkLight
-            )}
-            href={githubLink}
-            target="blank"
-          >
-            Github
-          </a>
-        </div>
-      )}
     </div>
   );
 };
